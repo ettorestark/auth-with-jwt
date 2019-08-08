499c778a8f9f843f099a75a56a6f313e3cd1627f
Ettore <ettoreastark@gmail.com>
2019-08-07 23:15:58 -0500

    I added validation if the user already exists

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,10 @@ const { registerValidation } = require('../validation');
 router.post('/register', async (req, res) => {
 	const { error } = registerValidation(req.body);
 	if(error) return res.status(400).send(error.details[0].message);
+
+	//Checking if the user is already in the database
+	const emailExist = await User.findOne({ email: req.body.email });
+	if(emailExist) return res.status(400).send('Email already exists');
 	
 	const user = new User({
 		name: req.body.name,
