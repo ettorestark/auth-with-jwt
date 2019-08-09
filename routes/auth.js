e68d30f901ba94bd561f877df696ed47b75da7b0
Ettore <ettoreastark@gmail.com>
2019-08-09 15:00:15 -0500

    I added login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs')
-const { registerValidation } = require('../validation'); 
+const { registerValidation, loginValidation } = require('../validation'); 
 
 
 router.post('/register', async (req, res) => {
@@ -23,10 +24,30 @@ router.post('/register', async (req, res) => {
 	});
 	try {
 		const savedUser = await user.save();
-		res.send(savedUser);
+		res.send({
+			user: user._id
+		});
 	}catch(err) {
 		res.status(400).send(err);
 	}
+});
+
+//Login
+router.post('/login', async (req, res) => {
+	const { error } = loginValidation(req.body);
+	if(error) return res.status(400).send(error.details[0].message);
+
+	//Checking if the email exists
+	const user = await User.findOne({ email: req.body.email });
+	if(!user) return res.status(400).send('Email or password is wrong');	
+
+	//Password is correct 
+	const validPass = await bcrypt.compare(req.body.password, user.password);
+	if(!validPass) return res.status(400).send('Email or password is wrong');
+
+	//Create and assign a token 
+	const token = jwt.sign({ _id: user._id }, 'secretPassword');
+	res.header('auth-token', token).send(token);
 });
 
 
